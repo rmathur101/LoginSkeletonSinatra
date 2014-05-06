e8a5f76c95c1c5981d6f29edd0a5ed4828032eed
Rohan Mathur <rmathur101@gmail.com>
2014-05-05 20:54:14 -0500

    Added new controller: user.rb

---
+++ app/controllers/user.rb
@@ -0,0 +1,11 @@
+before '/user/*' do
+	#DOES THIS EVEN DO ANYTHING?
+	if session[:user_id] != (User.find(session[:user_id])).id
+		redirect '/' #return to root because you are not supposed to be here
+	end
+
+end
+
+get '/user/home' do
+	erb :user_home
+end
